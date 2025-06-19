2be3cc0285a4a401480fcfd2f080f76edd4b5473
FloAiWb <kurunedam8@gmail.com>
2025-06-19 16:39:51 +0300

    Update order-list.tsx

--- src/containers/order/components/order-list.tsx
+++ src/containers/order/components/order-list.tsx
@@ -1,64 +1,69 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-/* eslint-disable react/jsx-wrap-multilines */
-/* eslint-disable prettier/prettier */
-// eslint-disable-next-line object-curly-newline
+// src/components/OrderList.tsx
+
+import React from "react";
 import { Order } from "@framework/types";
 import { addCommas } from "@persian-tools/persian-tools";
 import { Divider, List } from "antd";
 import { Link } from "react-router-dom";
+import t from "@/i18n/ru.json";
 
 interface Props {
   loading: boolean;
   orders: Order | undefined;
 }
 
-function OrderList({ loading, orders }: Props) {
+const OrderList: React.FC<Props> = ({ loading, orders }) => {
   return (
     <>
       <List
         loading={loading}
         itemLayout="horizontal"
-        // loadMore={loadMore}
         dataSource={orders?.order_Items}
         renderItem={(item) => (
           <List.Item>
             <List.Item.Meta
               title={
                 <Link to={`/products/${item.product_Id}`}>
-                  نام محصول :
+                  {t.productTitle}
                   <br />
                   {item.product_Name}
                 </Link>
               }
             />
 
-            <div className="flex flex-col gap-3  ">
-              <div className="flex flex-row-reverse gap-1">
-                <span>تومان</span>
+            <div className="flex flex-col gap-3">
+              {/* Цена за единицу */}
+              <div className="flex justify-end gap-1">
                 <span>{addCommas(item.final_Price)}</span>
-                <span> قیمت واحد :</span>
-              </div>
-              <div className="flex flex-row-reverse gap-1">
-                <span>عدد</span>
-                <span>{item.quantity}</span>
-                <span> تعداد :</span>
+                <span>{t.currency}</span>
+                <span>{t.unitPrice}</span>
               </div>
 
-              <div className="flex flex-row-reverse gap-1">
-                <span>تومان</span>
+              {/* Количество */}
+              <div className="flex justify-end gap-1">
+                <span>{item.quantity}</span>
+                <span>{t.qty}</span>
+              </div>
+
+              {/* Сумма по позиции */}
+              <div className="flex justify-end gap-1">
                 <span>{addCommas(item.final_Price * item.quantity)}</span>
-                <span> قیمت کل :</span>
+                <span>{t.currency}</span>
+                <span>{t.totalPriceLabel}</span>
               </div>
             </div>
           </List.Item>
         )}
       />
-      <Divider> مجموع قیمت </Divider>
-      <div>
-        <span>{addCommas(orders?.total_Price || 0)}</span> <span>تومان</span>
+
+      {/* Итого по всем позициям */}
+      <Divider>{t.total}</Divider>
+      <div className="flex justify-end gap-1">
+        <span>{addCommas(orders?.total_Price || 0)}</span>
+        <span>{t.currency}</span>
       </div>
     </>
   );
-}
+};
 
 export default OrderList;
