bc4bf4ceea9284046bab0b72cb6be6a57dd598ce
FloAiWb <kurunedam8@gmail.com>
2025-06-19 16:46:07 +0300

    Update header.tsx

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,23 +1,30 @@
+// src/components/AppHeader.tsx
+
+import React from "react";
 import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import t from "@/i18n/ru";
 
-function AppHeader() {
+const AppHeader: React.FC = () => {
   return (
-    <div className="flex w-full items-center justify-between rounded-lg ">
+    <div className="flex w-full items-center justify-between rounded-lg">
       <Link
-        className="flex items-center gap-2 rounded-lg  bg-[var(--tg-theme-secondary-bg-color)]  p-3"
-        to="/cart">
-        <ShoppingCartOutlined style={{ fontSize: "22px" }} />
-        <span>سبد خرید من</span>
+        to="/cart"
+        className="flex items-center gap-2 rounded-lg bg-[var(--tg-theme-secondary-bg-color)] p-3"
+      >
+        <ShoppingCartOutlined style={{ fontSize: 22 }} />
+        <span>{t.myCart}</span>
       </Link>
+
       <Link
-        className="flex items-center gap-2 rounded-lg  bg-[var(--tg-theme-secondary-bg-color)]  p-3"
-        to="/profile/home">
-        <span>حساب کاربری</span>
-        <UserOutlined style={{ fontSize: "22px" }} />
+        to="/profile/home"
+        className="flex items-center gap-2 rounded-lg bg-[var(--tg-theme-secondary-bg-color)] p-3"
+      >
+        <span>{t.userAccount}</span>
+        <UserOutlined style={{ fontSize: 22 }} />
       </Link>
     </div>
   );
-}
+};
 
 export default AppHeader;
