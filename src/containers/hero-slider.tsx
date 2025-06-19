5615a7a34c1cdad1fb801ce6b0f6b366e0703697
FloAiWb <kurunedam8@gmail.com>
2025-06-19 16:44:14 +0300

    Update hero-slider.tsx

--- src/containers/hero-slider.tsx
+++ src/containers/hero-slider.tsx
@@ -1,24 +1,32 @@
+// src/components/HeroSlider.tsx
+
+import React from "react";
 import { useGetSliders } from "@framework/api/slider/get";
 import { Carousel } from "antd";
+import t from "@/i18n/ru";
 
-function HeroSlider() {
+const HeroSlider: React.FC = () => {
   const { data } = useGetSliders();
-  return data?.length === 0 ? (
-    <span />
-  ) : (
+
+  if (!data || data.length === 0) {
+    return null;
+  }
+
+  return (
     <Carousel rootClassName="rounded-lg overflow-hidden" autoplay>
-      {data?.map((item, idx) => (
-        <div className="h-[160px] w-full">
+      {data.map((item, idx) => (
+        <div key={idx} className="h-[160px] w-full">
           <a href={item.url}>
             <img
               src={`${import.meta.env.VITE_API_URL}/${item.photo_Path}`}
-              alt="slider"
+              alt={t.sliderImageAlt}
+              className="w-full h-full object-cover"
             />
           </a>
         </div>
       ))}
     </Carousel>
   );
-}
+};
 
 export default HeroSlider;
