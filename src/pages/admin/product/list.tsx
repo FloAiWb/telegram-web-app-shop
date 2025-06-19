4df0a94a1d2f3ab4c14a535451e8afcd4e2df59a
FloAiWb <kurunedam8@gmail.com>
2025-06-19 16:51:52 +0300

    Update list.tsx

--- src/pages/admin/product/list.tsx
+++ src/pages/admin/product/list.tsx
@@ -1,38 +1,25 @@
-/* eslint-disable no-nested-ternary */
-/* eslint-disable object-curly-newline */
+// src/pages/admin/products.tsx
+
+import React from "react";
 import Container from "@components/container";
 import ProductLists from "@components/product/list";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
+import t from "@/i18n/ru";
 
-function ProductList() {
+const ProductList: React.FC = () => {
   const navigate = useNavigate();
-  // const { data, error, refetch, isLoading, isFetching } = useGetProducts({});
-  // useEffect(() => {
-  //   refetch();
-  // }, []);
 
   return (
     <Container
-      title="محصولات"
+      title={t.products}
       backwardUrl="/admin"
       customButton
-      customButtonTitle="افزودن"
-      customButtonOnClick={() => navigate("/admin/products/add")}>
-      {/* <Suspense fallback={<ProductsSkeleton />}> */}
-      {/* {isLoading || isFetching ? (
-        <ProductsSkeleton />
-      ) : error ? (
-        <>مشکلی رخ داده</>
-      ) : data?.products.length === 0 ? (
-        <Empty description="اطلاعاتی موجود نیست" />
-      ) : (
-        <ProductLists pageType="admin" data={data} />
-      )} */}
+      customButtonTitle={t.add}
+      customButtonOnClick={() => navigate("/admin/products/add")}
+    >
       <ProductLists pageType="admin" />
-
-      {/* </Suspense> */}
     </Container>
   );
-}
+};
 
 export default ProductList;
