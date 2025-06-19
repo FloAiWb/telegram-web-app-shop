af2ac92dd59c78e7e016b82f61cc08f9f3fa371e
FloAiWb <kurunedam8@gmail.com>
2025-06-19 15:16:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
-/** @type {import('vite').UserConfig} */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";          // ✨ добавляем
+import { resolve } from "path";     // ← добавьте
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {                             // ✨ новый блок
+  plugins: [react()],
+  resolve: {
     alias: {
-      "@": resolve(__dirname, "src")     // теперь @/ указывает на каталог src
+      "@": resolve(__dirname, "src") // ← @ указывает на каталог src
     }
-  },
-  server: {
-    open: false,
-    port: 3000
-  },
-  build: {
-    minify: "terser"
   }
 });
