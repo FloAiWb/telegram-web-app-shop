f106839de3a7c0b1dbd8687a586c5725895ee533
FloAiWb <kurunedam8@gmail.com>
2025-06-19 16:43:09 +0300

    Update 404.tsx

--- src/containers/404.tsx
+++ src/containers/404.tsx
@@ -1,16 +1,32 @@
-import { Button, Divider } from "antd";
-import { useNavigate } from "react-router";
+// src/components/NotFoundPage.tsx
 
-function NotFoundPage() {
+import React from "react";
+import { Button, Divider } from "antd";
+import { useNavigate } from "react-router-dom";
+import t from "@/i18n/ru";
+
+const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
+
   return (
-    <div className="flex h-[88vh] flex-col justify-center gap-10 rounded-lg  border-[1px] border-[var(--tg-theme-button-color)] p-3 py-6">
-      <div className="text-[50px]">404</div>
+    <div
+      className="flex h-[88vh] flex-col justify-center gap-10 rounded-lg
+                 border border-[var(--tg-theme-button-color)] p-6 text-center"
+    >
+      {/* Код ошибки */}
+      <div className="text-[50px] font-bold">{t.notFoundTitle}</div>
+
       <Divider />
-      <div>اوه به نظر به جای اشتباهی اومدی میتونی راحت بگردی به صفحه اصلی</div>
-      <Button onClick={() => navigate("/")}> بازگشت</Button>
+
+      {/* Описание */}
+      <div className="text-base">{t.notFoundDescription}</div>
+
+      {/* Кнопка возврата */}
+      <Button type="primary" onClick={() => navigate("/")}>
+        {t.back}
+      </Button>
     </div>
   );
-}
+};
 
 export default NotFoundPage;
